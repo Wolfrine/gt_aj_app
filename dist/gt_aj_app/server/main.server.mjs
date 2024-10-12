5e2a81c86cf91022fde9e2c33e81dbc096322d73
Wolfrine <schttewary@gmail.com>
2024-10-12 20:30:31 +0530

    App icon updates and few path corrections

--- dist/gt_aj_app/server/main.server.mjs
+++ dist/gt_aj_app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{c as a}from"./chunk-TMNB72XT.mjs";import"./chunk-2ZFNL5NB.mjs";import"./chunk-MAPOMOMH.mjs";import"./chunk-GBXDRUAU.mjs";export{a as default};
+import{c as a}from"./chunk-5MM5SHJZ.mjs";import"./chunk-2ZFNL5NB.mjs";import"./chunk-MAPOMOMH.mjs";import"./chunk-GBXDRUAU.mjs";export{a as default};
